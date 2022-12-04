8602a78776ae245fbf8aa6b15d52de3ade5a97b5
李昂軒(Ang-Hsuan Lee) <ahlee.shawn@gmail.com>
2022-12-04 13:46:23 -0500

    Update Rules.js

--- blokus/src/containers/Rules/Rules.js
+++ blokus/src/containers/Rules/Rules.js
@@ -5,9 +5,9 @@ const Rules = () => {
     return (
         <div>
             <h2 className="rules_title">Rules</h2>
-            <p>Press the space bar to rotate the selected chess piece by 90 degrees clockwise.</p>
-            <p>Press the arrow left or right keys to flip the selected chess piece horizontally.</p>
-            <p>Press the arrow up or bottom keys to flip the selected chess piece vertically.</p>
+            <p>Press the space bar to rotate the selected piece by 90 degrees clockwise.</p>
+            <p>Press the arrow left or right keys to flip the selected piece horizontally.</p>
+            <p>Press the arrow up or bottom keys to flip the selected piece vertically.</p>
         </div>
     );
 }
